89c293d64bc87590a3f70f8d5b2c1ae8d271d43b
Eugene Choi <echoi7@nd.edu>
2020-05-31 21:16:13 -0700

    Set up navigation

---
+++ src/Nav/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Nav() {
+    return (
+        <nav>
+            <li>
+                <ul><h3>Today</h3></ul>
+                <ul><h3>Calendar</h3></ul>
+                <ul><h3>Settings</h3></ul>
+                <ul><h3>Add Record</h3></ul>
+            </li>
+        </nav>
+    )
+}
